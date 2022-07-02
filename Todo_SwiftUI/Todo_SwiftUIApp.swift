a435e62ab3fe52640ef2f775a337d82e771d8b87
Andre Rodrigues <andrecomerciale@gmail.com>
2022-07-02 15:03:25 -0300

    Initial Commit

---
+++ Todo_SwiftUI/Todo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_SwiftUIApp.swift
+//  Todo_SwiftUI
+//
+//  Created by André Almeida on 2022-07-02.
+//
+
+import SwiftUI
+
+@main
+struct Todo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
